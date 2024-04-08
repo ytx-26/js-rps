e33077aac5d32a8250ac2bca0df6a70f09c8550c
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-04-08 18:49:12 +0100

    Fixed syntax

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function getComputerChoice(){
     let computerChoice = randomIndex[choices];
     return computerChoice;
 }
-const CompChoice = getComputerChoice()
-console.log(Compchoice);
+const CompChoice = getComputerChoice() //calls the function and stores the result in CompChoice
+console.log(CompChoice);
