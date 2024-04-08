2c91929dd1690816a789bfb9a15c92d30b1474ca
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-04-08 18:38:12 +0100

    Added function to get computer's choice.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice(){
     const choices = ['Rock', 'Paper', 'Scissors']
     const randomIndex = Math.floor(Math.random * (choices.length));
-    const computerChoice = randomIndex[choices]
+    let computerChoice = randomIndex[choices];
+    return computerChoice;
 }
