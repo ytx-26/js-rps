40276bbc5588547ced3a4ab2393641a3d38565d3
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-04-14 12:17:12 +0100

    Removed the = as it isn't needed

--- script.js
+++ script.js
@@ -25,11 +25,9 @@ for (let i = 0; i<5; i++){
     (PlayerChoice ==='paper' && ComputerChoice==='rock') ||
     (PlayerChoice ==='scissors' && ComputerChoice==='paper')){
         console.log('You win!!');
-        score = ++;
     }
     else{
         console.log('You lose');
-        score = --;
     }
 }
 
