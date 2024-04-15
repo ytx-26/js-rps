05caaa91427928d8b890ce32f6181a79c0fde2df
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-04-15 19:12:38 +0100

    Removed logic to prepare for UI

--- script.js
+++ script.js
@@ -10,28 +10,3 @@ function PlayerSelection(){
     return PlayerChoice; //returns whatever the player inputs
 }
 
-let score = 1;
-
-for (let i = 0; i<5; i++){
-    let PlayerChoice = PlayerSelection();
-    let ComputerChoice = getComputerChoice();
-    console.log("Your pick: ", PlayerChoice);
-    console.log("Computer's pick: ",ComputerChoice);
-
-    if (ComputerChoice===PlayerChoice){
-        console.log('Draw!!');
-    } else if (
-    (PlayerChoice ==='rock' && ComputerChoice==='scissors') ||
-    (PlayerChoice ==='paper' && ComputerChoice==='rock') ||
-    (PlayerChoice ==='scissors' && ComputerChoice==='paper')){
-        console.log('You win!!');
-        score++;
-    }
-    else{
-        console.log('You lose');
-        score--;
-    }
-}
-console.log('Your final score is: ',score);
-
-
